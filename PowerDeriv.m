03178b06335a6eacc6883fb1c899d75b67ee7a6b
SangminLee <snuvistasy@gmail.com>
2021-07-31 14:56:38 +0900

    Add FunDeriv class and power, exp, log functions (#3)

---
+++ PowerDeriv.m
@@ -0,0 +1,64 @@
+classdef PowerDeriv < FunDeriv
+    properties
+        % r: power (real number)
+        r
+    end
+    methods
+        function obj = PowerDeriv(r)
+            obj = obj@FunDeriv();
+            obj.r = r;
+        end
+        
+        % implement
+        function out = deriv(obj, x, n)
+            coeff = nan(n + 1, 1);
+            basis = nan(n + 1, 1);
+            
+            coeffTemp = 1;
+            basisTemp = power(x, obj.r - n);
+            
+            for k = 0:n
+                coeff(k + 1) = coeffTemp;
+                basis(n + 1 - k) = basisTemp;
+                
+                coeffTemp = coeffTemp*(obj.r - k);
+                basisTemp = basisTemp*x;
+            end
+            out = num2cell(coeff.*basis);
+        end
+    end
+    
+    methods(Static)
+        function test()
+            clc
+            
+            fprintf("== Test for PowerDeriv == \n")
+            power = PowerDeriv(3);
+            derivValues = power.deriv(1, 3);
+            fprintf("derivValues: \n")
+            disp(cell2mat(derivValues).')
+            
+            fprintf("1. Using forward methods \n")
+            t = 2;
+            xVar = DerivVariable(...
+                [1; t], ...
+                [0; 1], ...
+                [0; 0]);
+            xTVar = DerivVariable(...
+                [1, t], ...
+                [0, 1], ...
+                [0, 0]);
+            normSqVar = DerivMtimes(xTVar, xVar).forward();
+            normVar = PowerDeriv(1/2).forward(normSqVar);
+            fprintf("[normVar.deriv(0), normVar.deriv(1), normVar.deriv(2)]: \n")
+            disp([normVar.deriv(0), normVar.deriv(1), normVar.deriv(2)])
+            
+            fprintf("2. Analytic result \n")
+            z = (1 + t^2)^(1/2);
+            z_dot = t/(1 + t^2)^(1/2);
+            z_2dot = 1/(1 + t^2)^(3/2);
+            fprintf("[deriv(0), deriv(1), deriv(2)]: \n")
+            disp([z, z_dot, z_2dot])
+        end
+    end
+end
